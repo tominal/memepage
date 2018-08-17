737324e6abbda8821b1ca54245c4348784e0951a
tominal <tom@thomasj.me>
2018-08-17 18:00:48 -0500

    remove dump

--- inc/pdo.php
+++ inc/pdo.php
@@ -16,12 +16,13 @@ class conn {
     $this->user = $user;
     $this->pass = $pass;
 
-    var_dump($this->query("CREATE TABLE IF NOT EXISTS `users` (
+    // this is called every time :l i don't care.
+    $this->query("CREATE TABLE IF NOT EXISTS `users` (
       `id` int(11) auto_increment primary key not null,
       `name` varchar(50) not null,
       `email` varchar(50) not null,
       `scope` int(1) not null default 0
-    );"));
+    );");
   }
 
   public function query($sql){
