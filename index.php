2c46770fb9f7eb05bb9f31be062f7620cb9ed3b4
tominal <tom@thomasj.me>
2018-08-17 17:18:38 -0500

    google doesnt allow localhost auth :c

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  *
  */
 
-require_once(__DIR__.'/inc/config.php');
+require_once(__DIR__.'/inc/devconfig.php');
 require_once(__DIR__.'/inc/helpers.php');
 
 ?>
@@ -29,7 +29,6 @@ require_once(__DIR__.'/inc/helpers.php');
     <?php require(__DIR__.'/inc/nav.php'); ?>
     <div class="container">
       <?php page(); ?>
-      <?php var_dump(urlencode('!'));die(); ?>
     </div>
   </body>
 </html>
