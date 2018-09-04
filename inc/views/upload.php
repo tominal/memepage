b3863c4920dc1715769a4c83a897a669a4bc5b7c
= <=>
2018-09-04 10:53:41 -0500

    need json_contains

--- inc/views/upload.php
+++ inc/views/upload.php
@@ -57,7 +57,7 @@ $settings = $GLOBALS['settings'];
 	  };
 	  </script>
 
-	  <?php if($_SESSION['scope']){ $conn = $GLOBALS['conn']; $imgs = $conn->query("SELECT * FROM `images` WHERE tags is null OR tags = '[\"\"]';"); ?>
+	  <?php if($_SESSION['scope']){ $conn = $GLOBALS['conn']; $imgs = $conn->query("SELECT * FROM `images` WHERE (tags is null) OR (JSON_CONTAINS(tags, '[\"\"]'));"); ?>
 		<h3>Untagged memes</h3>
 		<?php if(!$imgs){ ?>
 		  <div class="alert alert-primary text-center">no images here!</div>
